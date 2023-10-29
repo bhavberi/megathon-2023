0c0607582d6f81af25f55cef25fc35fcadce9895
Soham Korade <sohamkorade.jnvkk@gmail.com>
2023-10-29 06:59:16 +0530

    make selenium work with docker

--- server/linkedin_skills.py
+++ server/linkedin_skills.py
@@ -3,9 +3,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('headless')
-chrome_options.add_argument('window-size=1920x1080')
-chrome_options.add_argument("disable-gpu")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1920,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=chrome_options)
 
 logged_in = False
