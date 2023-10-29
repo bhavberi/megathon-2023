f968ebb80cb42b5bed92e2a5e90e28fe63eb6ece
Soham Korade <sohamkorade.jnvkk@gmail.com>
2023-10-29 07:21:04 +0530

    headless chrome

--- server/linkedin_skills.py
+++ server/linkedin_skills.py
@@ -4,8 +4,8 @@ from selenium.webdriver.support import expected_conditions as EC
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--window-size=1920,1080')
-# chrome_options.add_argument('--headless')
+chrome_options.add_argument('--window-size=1920,1080')
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(options=chrome_options)
 
@@ -117,7 +117,7 @@ def load_creds():
 
 def init():
     # hide the browser window
-    # driver.set_window_position(-10000, 0)
+    driver.set_window_position(-10000, 0)
     email, password = load_creds()
     login(email, password)
 
